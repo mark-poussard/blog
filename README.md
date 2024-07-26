26d4b91c2179577bc46c81d675c7f1bd73c73e4d
Mark <poussard.mark@gmail.com>
2024-07-26 18:55:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Blog site
+
+Using the (eleventy example setup project)[https://github.com/11ty/eleventy-base-blog] by (Zach Leatherman)[https://zachleat.com/] 
+
+## Getting Started
+
+Install dependencies
+```
+npm i
+```
+
+Run the site locally
+```
+npm start
+```
+
+The blog will be automatically published to github page on master branch push
---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Mark's Blog",
+	url: "https://mark-poussard.github.io/blog/",
+	language: "en",
+	description: "I write about my experiences, thoughts and learnings as a software engineer.",
+	author: {
+		name: "Mark Poussard",
+		email: "poussard.mark@gmail.com",
+		url: "https://mark-poussard.github.io/blog/about/"
+	}
+}
